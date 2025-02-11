570029c4e3543784df8b89daa4c6086d1317c756
vincentiam <vincent03261996@gmail.com>
2025-02-11 23:04:15 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref} from 'vue'
+import {useRouter} from 'vue-router'
+const router = useRouter()
+const test = ref('')
+console.log(test.value)
+
+const login = () => {
+    if (test.value === '123') {
+        console.log('1')
+        router.push('/text')
+    }else{
+        console.log('2')
+    }
+}
+</script>
+<template>
+<input v-model="test">
+<br>
+<button @click="login">登入</button>
+</template>
+<style scoped></style>
