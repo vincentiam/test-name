034b1173b6346db5698edbfd55d078f3ee6025e2
vincentiam <vincent03261996@gmail.com>
2025-03-31 23:07:05 +0800

    3/31_3

--- pages/menunext/appointment_next/a4.vue
+++ pages/menunext/appointment_next/a4.vue
@@ -1,5 +1,8 @@
 <script setup >
 import { ref, computed } from 'vue';
+import { useToast } from 'primevue/usetoast';
+const toast = useToast();
+const supabase = useSupabaseClient();
 const name = ref(null)
 const birthday = ref(null)
 const id = ref(null)
@@ -8,13 +11,37 @@ const telephone = ref(null)
 const address = ref(null)
 const remark = ref(null)
 const date = ref(new Date())
-
+const telephoneSelected = ref('7')
 const submit = async() => {
     birthday.value = formatDate(birthday.value)
     date.value = formatDate(date.value)
     const { error } = await supabase.from('users').insert([
-        { 'users_id': id.value, 'users_name': name.value, 'users_birthday': birthday.value, 'users_phone': phone.value,  'users_address': address.value,  'users_remark': remark.value,  'users_telephone': telephone.value,  'users_date': date.value}
+        { 'users_id': id.value, 'users_name': name.value, 'users_birthday': birthday.value, 'users_phone': phone.value,  'users_address': address.value,  'users_remark': remark.value,  'users_telephone': telephone.value,  'users_start_date': date.value,  'users_last_date': date.value}
     ])
+    if (error) {
+        console.log(error)
+        toast.add({
+            severity: 'error',
+            summary: '登入錯誤',
+            life: 1500
+        })
+    } else {
+        name.value = null
+        birthday.value = null
+        id.value = null
+        phone.value = null
+        telephone.value = null
+        address.value = null
+        remark.value = null
+        date.value = new Date()
+        telephoneSelected.value = '7'
+        toast.add({
+            severity: 'success',
+            summary: '登入成功',
+            life: 1500
+        })
+    }
+    
 }
 
 const formatDate = (d) => {
@@ -37,7 +64,14 @@ const formatDate = (d) => {
             <DatePicker class="w-full" v-model="birthday" placeholder="出生年月日"/> 
             <InputText class="w-full" type="text" v-model="id" placeholder="身分證字號" maxlength="10"/>
             <InputMask class="w-full" type="text" v-model="phone" placeholder="手機號碼" mask="9999-999-999"/>
-            <InputMask class="w-full" type="text" v-model="telephone" placeholder="市話號碼" mask="(99)9999-9999"/>
+            <div class="flex flex-row gap-3 w-full">
+                <RadioButton v-model="telephoneSelected" inputId="rb1" value="7" />
+                <label for="rb1">7</label>
+                <RadioButton v-model="telephoneSelected" inputId="rb2" value="8" />
+                <label for="rb2">8</label>
+            </div>
+            <InputMask v-if="telephoneSelected==='7'" class="w-full" type="text" v-model="telephone" placeholder="市話號碼" mask="(99)999-9999"/>
+            <InputMask v-else class="w-full" type="text" v-model="telephone" placeholder="市話號碼" mask="(99)9999-9999"/>
             <InputText class="w-full" type="text" v-model="address" placeholder="地址"/>
             <InputText class="w-full" type="text" v-model="remark" placeholder="備註"/>
             <DatePicker class="w-full" v-model="date" placeholder="初診日期" />
